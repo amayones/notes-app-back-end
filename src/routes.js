420f970bfc6d9165aaacd7a90abf38c2fe83fdb7
amayones <mayonedrive1@gmail.com>
2025-08-01 13:53:03 +0700

    feat: implement create and detail notes functionality

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+const { addNotesHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+
+];
+
+module.exports = routes;
